5c3c75bfcc6bf43bcf3b47e5a18b5d71865cccc9
Moyuri Akther <akthermoyuri@.com>
2022-12-13 20:27:14 +0600

    add pay

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import Navbar from "./component/Pages/Shared/Navbar";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./component/Pages/Home/Home";
 import About from "./component/Pages/About/About";
 import NotFound from "./component/Pages/NotFound/NotFound";
